bca97b18b7971cee37dd69d96195f1bb67237d29
bigboss <bigboss@ima>
2020-09-08 17:20:41 +0300

    first stage of creating file from telemetry message.

--- rosbridge_ws_client.cpp
+++ rosbridge_ws_client.cpp
@@ -5,6 +5,7 @@
 #include <future>
 #include <string>
 #include <iostream>
+#include <time.h>
 
 #include "rapidjson/document.h" 
 
@@ -14,6 +15,10 @@ using namespace rapidjson;
 //RosbridgeWsClient rbc("simapi.easyrms.app:9090");
 //RosbridgeWsClient rbc("192.168.17.100:9091");
 RosbridgeWsClient rbc("simapi.easyrms.app:9091");
+
+
+FILE *Telemetry;
+
 
 void advertiseServiceCallback(std::shared_ptr<WsClient::Connection> /*connection*/, std::shared_ptr<WsClient::InMessage> in_message)
 {
@@ -64,9 +69,209 @@ void subscriberCallback(std::shared_ptr<WsClient::Connection> connection, std::s
   std::cout << "subscriberCallback(): Drone mode " << in_message->string() << std::endl;
 }
 
+int  GetTimeFromMidnight()
+{
+  // double Total = 0.0;
+  // time_t rawtime;
+  // struct tm * timeinfo;
+
+        // struct timeval tv;
+        // struct timezone tz;
+        // struct tm *tm;
+        // gettimeofday(&tv,&tz);
+        // tm=localtime(&tv.tv_sec);
+        // printf("StartTime: %d:%02d:%02d %d \n", tm->tm_hour, tm->tm_min, tm->tm_sec, tv.tv_usec);
+
+
+
+  // time ( &rawtime );
+  // timeinfo = localtime ( &rawtime );
+  // Total = timeinfo->tm_sec + (timeinfo->tm_min * 60) + (timeinfo->tm_hour * 3600);
+  // printf("\nCurrent local time and date: %f",Total);// asctime (timeinfo) );
+
+//  auto start = std::chrono::system_clock::now();
+//  unsigned int Time = 0;
+//  time_t st;
+//  ctime(&st);
+//
+//  Time = st->
+
+    timespec time1;
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+
+    return ((int)time1.tv_nsec);
+
+
+}
 void telemetryCallback(std::shared_ptr<WsClient::Connection> connection, std::shared_ptr<WsClient::InMessage> in_message)
 {
-  std::cout << "ZZZ telemetryCallback():  " << in_message->string() << std::endl;
+  int TimeMsec = 0;
+  bool StartBrakeFlag = false;
+  bool EndBrakeFlag = false;
+  bool Go = true;
+  int i = 0;
+  int ComaCount = 0;
+  int Index = 0;
+  int DataIndex = 0;
+  char MsgString[500] = {0};
+  char MsgParam[50] = {0};
+  char MsgData[50] = {0};
+  float MsgDataFloat = 0.0;
+  double vertical_speed = 0.0;
+  double yaw = 0.0;
+  double pitch = 0.0;
+  double roll = 0.0;
+  double speed = 0.0;
+  double altitude = 0.0;
+  double longitude = 0.0;
+  double latitude = 0.0;
+  double current = 0.0;
+  double voltage = 0.0;
+
+//  std::cout << "ZZZ telemetryCallback():  " << in_message->string() << std::endl;
+
+//  strcpy(MsgString,(char *)in_message->string() );
+  strcpy(MsgString,in_message->string().c_str());
+//  printf("\n%s\n",MsgString);
+
+  i=0;
+
+  do
+  {
+    if(MsgString[i] == 125)
+      Go = false;
+    else
+    {
+      if(MsgString[i] == '{')
+      {
+        ComaCount++;
+      }
+      if(ComaCount == 2)
+      {
+        if(MsgString[i] == '"')
+        {
+          if(!StartBrakeFlag)
+          {
+            StartBrakeFlag = true;
+            EndBrakeFlag = false;
+            memset(MsgParam,0,50);
+            memset(MsgData,0,50);
+            Index = 0;
+          }
+          else
+          {
+            if(MsgString[i+1] == ':')
+            {
+              StartBrakeFlag = false;
+              EndBrakeFlag = true;
+              i++; // read ":"
+            }
+            else
+            {
+              StartBrakeFlag = true;
+              EndBrakeFlag = false;
+              memset(MsgParam,0,50);
+              memset(MsgData,0,50);
+              Index = 0;
+            }
+            
+          }
+            
+        }
+        else
+        {
+          // get the data
+          if(StartBrakeFlag)
+          {
+            MsgParam[Index] = MsgString[i];
+            Index++;
+          }
+          if(EndBrakeFlag)
+          {
+            EndBrakeFlag = false;
+            MsgParam[Index] = 0;
+            Index = 0;
+            i++;
+            DataIndex = 0;
+            while((MsgString[i] != ',') && (MsgString[i] != 125)) // '}'
+            {
+              MsgData[DataIndex] = MsgString[i];
+              i++;
+              DataIndex++;
+            }
+    //        if(MsgString[i] == 125)
+    //          Go = false;
+    //        else
+//            printf("\n%s",MsgData);
+            MsgData[DataIndex] = 0;
+            MsgDataFloat = atof(MsgData);
+  //          std::cout << ">>  " << MsgData << std::endl;
+            if(!strcmp(MsgParam,"vertical_speed"))
+            {
+              vertical_speed = MsgDataFloat;
+              printf("\n\n vertical_speed %f",vertical_speed);
+            }
+            if(!strcmp(MsgParam,"yaw"))
+            {
+              yaw = MsgDataFloat;
+              printf("\n yaw %f",yaw);
+            }
+            if(!strcmp(MsgParam,"pitch"))
+            {
+              pitch = MsgDataFloat;
+              printf("\n pitch %f",pitch);
+            }
+            if(!strcmp(MsgParam,"roll"))
+            {
+              roll = MsgDataFloat;
+              printf("\n roll %f",roll);
+            }
+            if(!strcmp(MsgParam,"speed"))
+            {
+              speed = MsgDataFloat;
+              altitude = MsgDataFloat;
+              printf("\n altitude %f",altitude);
+            }
+            if(!strcmp(MsgParam,"longitude"))
+            {
+              current = MsgDataFloat;
+              printf("\n current %f",current);
+            }
+            if(!strcmp(MsgParam,"voltage"))
+            {
+              voltage = MsgDataFloat;
+              printf("\n voltage %f",voltage);
+            }
+            if(!strcmp(MsgParam,"latitude"))
+            {
+              latitude = MsgDataFloat;
+              printf("\n latitude %f",latitude);
+            }
+          }
+        }
+      }
+    }
+    i++;
+  } while ((MsgString[i] != 0) && Go);
+  if(Telemetry != nullptr)
+  {
+    TimeMsec = GetTimeFromMidnight();
+    fprintf(Telemetry,"%d,%2.5f,%3.5f,%3.5f,%3.8f,%3.8f,%5.2f,%5.2f,%5.2f,%3.2f,%3.2f\n",
+            TimeMsec,
+            pitch,
+            roll,
+            yaw,
+            latitude,
+            longitude,
+            altitude,
+            vertical_speed,
+            speed,
+            current,
+            voltage);
+  }
+//  std::cout << ">>>>>>>>  " << "voltage " << voltage << std::endl;
+//  printf("\nlongitude = %f\n",longitude);
+
 }
 
 int main() {
@@ -75,7 +280,14 @@ int main() {
   rbc.subscribe("sub", "/control_api/drone/status", subscriberCallback);
   rbc.addClient("sub2");
   rbc.subscribe("sub2", "/control_api/drone/telemetry", telemetryCallback);
+  
+  Telemetry = fopen("Telemetry.txt", "wt");
+  if(Telemetry != nullptr)
+  {
+    fprintf(Telemetry,"TimeMsec,pitch,roll,yaw,latitude,longitude,altitude,vertical_speed,speed,current,voltage\n");
+  }
 
+  
   rapidjson::Document document1(rapidjson::kObjectType);
   document1.AddMember("type", "open", document1.GetAllocator());
   document1.AddMember("param1", 0, document1.GetAllocator());
@@ -83,6 +295,7 @@ int main() {
   rbc.callService("/control_api/eg/command", callServiceCallback, document1);
   usleep(10*1e6);
   
+/*
   rapidjson::Document document2(rapidjson::kObjectType);
   document2.AddMember("min_pitch", 0, document2.GetAllocator());
   document2.AddMember("yaw", 0, document2.GetAllocator());
@@ -96,10 +309,11 @@ int main() {
   document3.AddMember("altitude", 20, document3.GetAllocator());
   rbc.callService("/control_api/cmd/land", callServiceCallback, document3);
   usleep(30*1e6);
-
+*/
   rbc.removeClient("sub");
   std::cout << "Program terminated" << std::endl;
-	
+  if(Telemetry != nullptr)
+    fclose(Telemetry);
 
   
   //rbc.addClient("service_advertiser");
